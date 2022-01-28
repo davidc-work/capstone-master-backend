7ef3a39f607487c34181c5411d874de048310232
Wesley Tejeda <wtejeda@talentpath.com>
2022-01-28 12:47:10 -0500

    Added new routes from microservices

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ var router = express.Router();
 const https = require('https');
 const bodyParser = require('body-parser');
 const axios = require('axios').default;
+const { response } = require('express');
+const { Socket } = require('dgram');
 
 var jsonParser = bodyParser.json();
 
@@ -50,4 +52,89 @@ router.post('/purchase-fund', jsonParser, (req, res) => {
   })();
 });
 
+//GET all mutual funds with: associated stocks for mutual funds page where:
+// params = {
+//   id: Mutual Fund Id
+// }
+// Works //
+router.get("/mutual-funds", async (req,res) => {
+  console.log(req.body);
+  //staging api call
+  let funds = await axios.get("https://immense-brushlands-56087.herokuapp.com/funds").then(({ data }) => data).catch(err => err);
+  let stocks = await axios.get("http://stocks-microservice.herokuapp.com/stocks").then(({ data }) => data).catch(err => err);
+  // console.log(stocks);
+  funds.forEach(fund => {
+    //find and match each stocks that corresponds to each mutual fund
+    let stocksToSend = [];
+    stocks.forEach(stock => {
+      if(stock.mutualFundIds !== ""){
+        stock.mutualFundIds = stock.mutualFundIds.toString().split(",");
+        console.log(stock.mutualFundIds);
+        stock.mutualFundIds.includes(fund.id.toString()) ? stocksToSend.push(stock) : null;
+      }
+    })
+    fund.stocks = stocksToSend;
+  })
+  //Return data or response to frontend  
+  res.json(funds);
+});
+
+//GET all stocks for stock page
+// Works //
+router.get("/stocks", async (req,res) => {
+  console.log(req.body);
+  //staging api call
+  let stocks = await axios.get("http://stocks-microservice.herokuapp.com/stocks").then(({ data }) => data).catch(err => err);
+  //Return data or response to frontend  
+  res.json(stocks);
+});
+
+//GET specific user profile with: transactions & portfolio
+// Works //
+router.get("/users/:id", async (req,res) => {
+  console.log(req.body);
+  //staging api call
+  let profile = await axios.get(`http://user-profile-transaction.herokuapp.com/customer/${req.params.id}`).then(({ data }) => data).catch(err => err);
+  profile.transactions = await axios.get(`https://transaction-microservice-v1.herokuapp.com/customers/${req.params.id}`).then(({ data }) => data).catch(err => err);
+  //Return data or response to frontend
+  res.json(profile);
+});
+
+//POST a deposit transaction where:
+// req.body = {
+//   type: "deposit" (Must be lowercase string),
+//   amount: Integer,
+//   CustomerId, Integer
+// }
+// Works //
+router.post("/transactions/deposit", async (req,res) => {
+  console.log(req.body);
+  //staging api call
+  let temp = await axios.post("https://transaction-microservice-v1.herokuapp.com/transactions/create", req.body).then(({ data }) => data).catch(err => err);
+  console.log(temp)
+  //Return data or response to frontend  
+  res.send(temp)
+});
+
+//POST a sell transaction where:
+// req.body = {
+//   type: "sell" (Must be lowercase string),
+//   id: Integer (Transaction id),
+//   quantity: Integer (Quantity to sell will fail if greater than available or if 0),
+//   CustomerId: Integer
+// }
+// Works //
+router.post("/transactions/sell", async (req,res) => {
+  console.log(req.body);
+  //staging api call
+  let fund = await axios.get("https://immense-brushlands-56087.herokuapp.com/funds/"+req.body.mutualFundId).then(({ data }) => data).catch(err => err);
+  req.body.itemDescription = fund.name;
+  req.body.mutualFundId = fund.id;
+  req.body.pricePerUnit = fund.price;
+  console.log(req.body);
+  let temp = await axios.post("https://transaction-microservice-v1.herokuapp.com/transactions/sell", req.body).then(({ data }) => data).catch(err => err);
+  console.log(temp)
+  //Return data or response to frontend  
+  res.json(temp)
+});
 module.exports = router;
